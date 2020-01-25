d01bf9f93ccc837370c09054b7f17043b56be159
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-01-25 23:05:31 +0000

    made the generate function static

--- src/main/java/com/effa/jwt/jwt/utils/Generator.java
+++ src/main/java/com/effa/jwt/jwt/utils/Generator.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
 public class Generator {
-    public String generate(String username){
+    public static String generate(String username){
         Algorithm algorithm = Algorithm.HMAC256("super secret");
         String token = JWT.create()
                 .withClaim("username", username)
