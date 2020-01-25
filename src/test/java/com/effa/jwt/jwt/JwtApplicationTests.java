341347369d2423e6002ca504a147b3dac498796f
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-01-25 19:43:25 +0000

    init

---
+++ src/test/java/com/effa/jwt/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effa.jwt.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
